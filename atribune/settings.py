780e2d77f20bad75dcae19ef18c8bccb094f6136
Richard Waweru <developer.waweru@gmail.com>
2018-12-14 23:38:55 +0300

    - Add build folder in gitignore

--- atribune/settings.py
+++ atribune/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS=['localhost', '.ngrok.io', '10.10.10.77', '.herokuapp.com', 'djreact-tribune.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS=['localhost', '.ngrok.io', '10.10.10.77', '.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
